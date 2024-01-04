889212c88f99db8af6a4cc28bc6604ef29d15fc1
dawid <dawidlasek137@gmail.com>
2024-01-04 16:37:43 +0100

    django templates for listing available topics and topic entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'learning_log/index.html')
+
+
+def topics(request):
+	all_topics = Topic.objects.order_by('date_added')
+	context = {'topics': all_topics}
+	return render(request, 'learning_log/topics.html', context)
+
+
+def topic(request, topic_id):
+	one_topic = Topic.objects.get(id=topic_id)
+	entries = one_topic.entry_set.order_by('-date_added')
+	context = {'topics': topics, 'entries': entries}
+	return render(request, 'learning_log/topic.html', context)
