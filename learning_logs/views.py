c2e093457f60efe361e471b0e2359008a68e0879
dawid <dawidlasek137@gmail.com>
2024-01-07 15:58:20 +0100

    adding new entry for topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'learning_log/index.html')
+	return render(request, 'learning_logs/index.html')
 
 
 def topics(request):
 	all_topics = Topic.objects.order_by('date_added')
 	context = {'topics': all_topics}
-	return render(request, 'learning_log/topics.html', context)
+	return render(request, 'learning_logs/topics.html', context)
 
 
 def topic(request, topic_id):
-	one_topic = Topic.objects.get(id=topic_id)
-	entries = one_topic.entry_set.order_by('-date_added')
-	context = {'topics': topics, 'entries': entries}
-	return render(request, 'learning_log/topic.html', context)
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic': topic, 'entries': entries}
+	return render(request, 'learning_logs/topic.html', context)
 
 
 def new_topic(request):
@@ -29,6 +29,21 @@ def new_topic(request):
 		form = TopicForm(data=request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect('learnings_logs:topics')
+			return redirect('learning_logs:topics')
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+	topic = Topic.objects.get(id=topic_id)
+	if request.method != 'POST':
+		form = EntryForm()
+	else:
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return redirect('learning_logs:topic', topic_id=topic_id)
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
